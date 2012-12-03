0749a31ca2309ffefbb05fdf37e535763ddcd63a
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-04 00:31:57 +0100

    Move main list adapter

--- src/com/gyp/pfc/adapters/MainListAdapter.java
+++ src/com/gyp/pfc/adapters/MainListAdapter.java
@@ -1,14 +1,20 @@
 /**
  * 
  */
-package com.gyp.pfc;
+package com.gyp.pfc.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.gyp.pfc.R;
+import com.gyp.pfc.listeners.AddButtonOnClickListener;
+import com.gyp.pfc.listeners.ListButtonOnClickListener;
 
 /**
  * @author Alvaro
@@ -22,6 +28,8 @@ public class MainListAdapter extends ArrayAdapter<String> {
 
 	class ViewHolder {
 		TextView title;
+		Button addButton;
+		Button listButton;
 	}
 
 	public MainListAdapter(Context context, int textViewResourceId,
@@ -42,6 +50,8 @@ public class MainListAdapter extends ArrayAdapter<String> {
 			holder = new ViewHolder();
 
 			holder.title = (TextView) v.findViewById(R.id.entryName);
+			holder.addButton = (Button) v.findViewById(R.id.entryAddButton);
+			holder.listButton = (Button) v.findViewById(R.id.entryListButton);
 			v.setTag(holder);
 		} else {
 			holder = (ViewHolder) v.getTag();
@@ -49,6 +59,10 @@ public class MainListAdapter extends ArrayAdapter<String> {
 		String str = data[position];
 
 		holder.title.setText(str);
+		OnClickListener listListener = new ListButtonOnClickListener(str);
+		holder.listButton.setOnClickListener(listListener);
+		OnClickListener addListener = new AddButtonOnClickListener(str);
+		holder.addButton.setOnClickListener(addListener);
 
 		return v;
 	}
