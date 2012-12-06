36dcb8f235461e43a74a1a1f9d018baa50f89ec9
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-06 12:28:40 +0100

    Add DatabaseConfigUtil for ORMLite to work

---
+++ src/com/gyp/pfc/data/db/DatabaseConfigUtil.java
@@ -0,0 +1,44 @@
+package com.gyp.pfc.data.db;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import com.gyp.pfc.data.domain.Food;
+import com.j256.ormlite.android.apptools.OrmLiteConfigUtil;
+
+/**
+ * Configuration class for the ORMLite configuration
+ * 
+ * @author Alvaro
+ * 
+ */
+public class DatabaseConfigUtil extends OrmLiteConfigUtil {
+
+	// Constants -----------------------------------------------------
+
+	// Attributes ----------------------------------------------------
+
+	// Static --------------------------------------------------------
+	/**
+	 * Which classes to use for the configuration writing
+	 */
+	private static final Class<?>[] classes = new Class[] { Food.class };
+
+	/**
+	 * This method must be launched with each modification to entity classes.
+	 */
+	public static void main(String[] args) throws SQLException, IOException {
+		writeConfigFile("ormlite_config.txt", classes);
+	}
+
+	// Constructors --------------------------------------------------
+
+	// Public --------------------------------------------------------
+	// Package protected ---------------------------------------------
+
+	// Protected -----------------------------------------------------
+
+	// Private -------------------------------------------------------
+
+	// Inner classes -------------------------------------------------
+}
