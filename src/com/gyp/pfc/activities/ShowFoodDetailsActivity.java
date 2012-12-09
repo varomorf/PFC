ff9a950ffdbb8e8439409b8a58772e5ed7ce1234
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-09 23:58:34 +0100

    Add menu with Edit and Delete options to the ShowFoodDetailsActivity.

--- src/com/gyp/pfc/activities/ShowFoodDetailsActivity.java
+++ src/com/gyp/pfc/activities/ShowFoodDetailsActivity.java
@@ -3,10 +3,15 @@ package com.gyp.pfc.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gyp.pfc.R;
+import com.gyp.pfc.data.db.DatabaseHelper;
 import com.gyp.pfc.data.domain.Food;
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 
 /**
  * Activity for showing the details of a {@link Food} entity
@@ -14,7 +19,8 @@ import com.gyp.pfc.data.domain.Food;
  * @author Alvaro
  * 
  */
-public class ShowFoodDetailsActivity extends Activity {
+public class ShowFoodDetailsActivity extends
+		OrmLiteBaseActivity<DatabaseHelper> {
 
 	// Constants -----------------------------------------------------
 
@@ -40,14 +46,53 @@ public class ShowFoodDetailsActivity extends Activity {
 		// retrieve the food to be shown from the intent
 		Intent sender = getIntent();
 		food = (Food) sender.getExtras().getSerializable(
-				FoodsListActivity.SELECTED_FOOD);
+				EditFoodActivity.FOOD_TO_EDIT);
 		getUIForms();
 		updateUI();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.foods_list_context_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.deleteFood:
+			getHelper().getFoodDao().delete(food);
+			finish();
+			return true;
+		case R.id.editFood:
+			editFood();
+			return true;
+		default:
+			return super.onContextItemSelected(item);
+		}
 	}
 
 	// Package protected ---------------------------------------------
 
 	// Protected -----------------------------------------------------
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case EditFoodActivity.EDIT_FOOD:
+			if (resultCode == Activity.RESULT_OK) {
+				// get updated food from intent
+				food = (Food) data.getExtras().get(
+						EditFoodActivity.FOOD_TO_EDIT);
+				// refresh UI
+				updateUI();
+				Toast.makeText(getApplicationContext(),
+						getString(R.string.editFoodMessage), Toast.LENGTH_SHORT)
+						.show();
+			}
+			break;
+		}
+	}
 
 	// Private -------------------------------------------------------
 	private void getUIForms() {
@@ -68,6 +113,13 @@ public class ShowFoodDetailsActivity extends Activity {
 		fatsText.setText(food.getFats() + " gr. / " + food.getFatsCalories()
 				+ " Kcal - " + food.getFatsPercentage() + "%");
 		fatsText.setBackgroundColor(food.getFatsColor());
+	}
+
+	private void editFood() {
+		Intent editFoodIntent = new Intent(getApplicationContext(),
+				EditFoodActivity.class);
+		editFoodIntent.putExtra(EditFoodActivity.FOOD_TO_EDIT, food);
+		startActivityForResult(editFoodIntent, EditFoodActivity.EDIT_FOOD);
 	}
 	// Inner classes -------------------------------------------------
 
