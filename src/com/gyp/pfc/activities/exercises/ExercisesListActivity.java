ed45bd65b4d400c986a4e6c93ecb38f20af5351d
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-27 20:16:33 +0100

    H1: add ExerciseListActivity backbone

---
+++ src/com/gyp/pfc/activities/exercises/ExercisesListActivity.java
@@ -0,0 +1,39 @@
+package com.gyp.pfc.activities.exercises;
+
+import android.os.Bundle;
+
+import com.gyp.pfc.data.db.DatabaseHelper;
+import com.j256.ormlite.android.apptools.OrmLiteBaseListActivity;
+
+/**
+ * Activity for listing exercises and interacting with them
+ * 
+ * @author Alvaro
+ * 
+ */
+public class ExercisesListActivity extends
+		OrmLiteBaseListActivity<DatabaseHelper> {
+
+	// Constants -----------------------------------------------------
+
+	// Attributes ----------------------------------------------------
+
+	// Static --------------------------------------------------------
+
+	// Constructors --------------------------------------------------
+
+	// Public --------------------------------------------------------
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// TODO
+	}
+
+	// Package protected ---------------------------------------------
+
+	// Protected -----------------------------------------------------
+
+	// Private -------------------------------------------------------
+
+	// Inner classes -------------------------------------------------
+}
