8bd54cd2223ea220a59d2eb54002a10d37e3fde5
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-27 20:07:54 +0100

    H1: add domain entity for an exercise

---
+++ src/com/gyp/pfc/data/domain/Exercise.java
@@ -0,0 +1,67 @@
+package com.gyp.pfc.data.domain;
+
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+/**
+ * Exercise entity
+ * 
+ * @author Alvaro
+ * 
+ */
+@DatabaseTable
+public class Exercise {
+
+	// Constants -----------------------------------------------------
+
+	// Attributes ----------------------------------------------------
+	@DatabaseField
+	int id;
+	@DatabaseField
+	String name;
+	@DatabaseField
+	String description;
+
+	// Static --------------------------------------------------------
+
+	// Constructors --------------------------------------------------
+
+	// Public --------------------------------------------------------
+	/**
+	 * ORMLite needs a no-arg constructor
+	 */
+	public Exercise() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	// Package protected ---------------------------------------------
+
+	// Protected -----------------------------------------------------
+
+	// Private -------------------------------------------------------
+
+	// Inner classes -------------------------------------------------
+}
