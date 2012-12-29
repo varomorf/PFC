91eccdac85cc5d219558470e042f66126726205f
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-29 20:29:04 +0100

    h5: remove unneded import

--- src/com/gyp/pfc/activities/exercise/ExerciseDetailsActivity.java
+++ src/com/gyp/pfc/activities/exercise/ExerciseDetailsActivity.java
@@ -3,11 +3,10 @@ package com.gyp.pfc.activities.exercise;
 import android.os.Bundle;
 
 import com.gyp.pfc.data.db.DatabaseHelper;
-import com.gyp.pfc.data.domain.Exercise;
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 
 /**
- * Activity for showing the details of a {@link Exercise} entity
+ * Activity for showing the details of an Exercise entity
  * 
  * @author Alvaro
  * 
