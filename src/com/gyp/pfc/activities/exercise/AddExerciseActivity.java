f7b637c3c19e396e16ec83cc8c716a42f1471771
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-30 10:47:06 +0100

    h2: add backbone for AddFoodActivity

---
+++ src/com/gyp/pfc/activities/exercise/AddExerciseActivity.java
@@ -0,0 +1,31 @@
+package com.gyp.pfc.activities.exercise;
+
+import com.gyp.pfc.data.db.DatabaseHelper;
+import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
+
+/**
+ * Activity for adding new exercises
+ * 
+ * @author Alvaro
+ * 
+ */
+public class AddExerciseActivity extends OrmLiteBaseActivity<DatabaseHelper> {
+
+	// Constants -----------------------------------------------------
+
+	// Attributes ----------------------------------------------------
+
+	// Static --------------------------------------------------------
+
+	// Constructors --------------------------------------------------
+
+	// Public --------------------------------------------------------
+
+	// Package protected ---------------------------------------------
+
+	// Protected -----------------------------------------------------
+
+	// Private -------------------------------------------------------
+
+	// Inner classes -------------------------------------------------
+}
