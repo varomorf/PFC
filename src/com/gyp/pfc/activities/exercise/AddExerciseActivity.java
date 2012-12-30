d3f756c110d40b48efeb0a64ce08a01d80613fd3
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-30 18:01:48 +0100

    h2: now affirmative toast is shown after adding exercise and text fields get cleared

--- src/com/gyp/pfc/activities/exercise/AddExerciseActivity.java
+++ src/com/gyp/pfc/activities/exercise/AddExerciseActivity.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang.StringUtils;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import com.gyp.pfc.R;
@@ -48,33 +47,42 @@ public class AddExerciseActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	 * @param view
 	 */
 	public void addExercise(View view) {
+		View name = findViewById(R.id.exerciseName);
+		View description = findViewById(R.id.exerciseDescription);
 		Exercise exercise = new Exercise();
-		exercise.setName(UIUtils.getTextFromUI(findViewById(R.id.exerciseName)));
+		exercise.setName(UIUtils.getTextFromUI(name));
 		if (assertExercise(exercise)) {
-			exercise.setDescription(UIUtils
-					.getTextFromUI(findViewById(R.id.exerciseDescription)));
+			exercise.setDescription(UIUtils.getTextFromUI(description));
 			getHelper().getExerciseDao().create(exercise);
+			Toast.makeText(getApplicationContext(), R.string.exerciseCreated,
+					Toast.LENGTH_SHORT).show();
+			UIUtils.clearView(name);
+			UIUtils.clearView(description);
 		}
 	}
 
 	// Private -------------------------------------------------------
-	private boolean assertExercise(Exercise exercise){
-		return assertNotBlankName(exercise) && assertNotDuplicatedName(exercise);
+	private boolean assertExercise(Exercise exercise) {
+		return assertNotBlankName(exercise)
+				&& assertNotDuplicatedName(exercise);
 	}
-	
+
 	private boolean assertNotBlankName(Exercise exercise) {
 		if (StringUtils.isBlank(exercise.getName())) {
 			Toast.makeText(getApplicationContext(), R.string.exerciseNameBlank,
 					Toast.LENGTH_SHORT).show();
+			return false;
 		}
 		return true;
 	}
-	
-	private boolean assertNotDuplicatedName(Exercise exercise){
-		List<Exercise> tmp = getHelper().getExerciseDao().queryForEq("name", exercise.getName());
+
+	private boolean assertNotDuplicatedName(Exercise exercise) {
+		List<Exercise> tmp = getHelper().getExerciseDao().queryForEq("name",
+				exercise.getName());
 		if (tmp.size() != 0) {
-			Toast.makeText(getApplicationContext(), R.string.exerciseNameDuplicated,
-					Toast.LENGTH_SHORT).show();
+			Toast.makeText(getApplicationContext(),
+					R.string.exerciseNameDuplicated, Toast.LENGTH_SHORT).show();
+			return false;
 		}
 		return true;
 	}
