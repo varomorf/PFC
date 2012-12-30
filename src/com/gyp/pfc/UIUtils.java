523be65f984878cdafdd449daaa0bcf987e4d688
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-30 18:13:48 +0100

    h2: comments

--- src/com/gyp/pfc/UIUtils.java
+++ src/com/gyp/pfc/UIUtils.java
@@ -16,21 +16,45 @@ public class UIUtils {
 	// Attributes ----------------------------------------------------
 
 	// Static --------------------------------------------------------
+	/**
+	 * <p>
+	 * Returns the String representation of the text held by the passed view as
+	 * long as this view is a {@link TextView}
+	 * </p>
+	 * 
+	 * @param view
+	 *            The view from which to extract the text
+	 * @return The extracted text of null if the view is null or not
+	 *         {@link TextView}
+	 */
 	public static String getTextFromUI(View view) {
+		// null will be returned if the view is not valid
 		String text = null;
 		if (validTextView(view)) {
+			// get the text and convert to string
 			text = ((TextView) view).getText().toString();
 		}
 		return text;
 	}
 
+	/**
+	 * Clears the text of the passed view as long as it's a {@link TextView}
+	 * 
+	 * @param view
+	 *            The view to clear its text
+	 */
 	public static void clearView(View view) {
 		if (validTextView(view)) {
+			// clear the view setting empty string
 			((TextView) view).setText("");
 		}
 	}
 
 	// Constructors --------------------------------------------------
+
+	private UIUtils() {
+		// NOOP
+	}
 
 	// Public --------------------------------------------------------
 
@@ -41,6 +65,7 @@ public class UIUtils {
 	// Private -------------------------------------------------------
 
 	private static boolean validTextView(View view) {
+		// view is valid if not null and is a TextView
 		return view != null && view instanceof TextView;
 	}
 
