0a1ba7ff259ad6b86337bc09118beeec5f121778
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2012-12-30 15:05:09 +0100

    h2: finished implementing AddExerciseActivity

--- src/com/gyp/pfc/activities/exercise/AddExerciseActivity.java
+++ src/com/gyp/pfc/activities/exercise/AddExerciseActivity.java
@@ -1,6 +1,18 @@
 package com.gyp.pfc.activities.exercise;
 
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.gyp.pfc.R;
+import com.gyp.pfc.UIUtils;
 import com.gyp.pfc.data.db.DatabaseHelper;
+import com.gyp.pfc.data.domain.Exercise;
 import com.j256.ormlite.android.apptools.OrmLiteBaseActivity;
 
 /**
@@ -24,8 +36,47 @@ public class AddExerciseActivity extends OrmLiteBaseActivity<DatabaseHelper> {
 	// Package protected ---------------------------------------------
 
 	// Protected -----------------------------------------------------
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add_exercise);
+	}
+
+	/**
+	 * Call back for the add button
+	 * 
+	 * @param view
+	 */
+	public void addExercise(View view) {
+		Exercise exercise = new Exercise();
+		exercise.setName(UIUtils.getTextFromUI(findViewById(R.id.exerciseName)));
+		if (assertExercise(exercise)) {
+			exercise.setDescription(UIUtils
+					.getTextFromUI(findViewById(R.id.exerciseDescription)));
+			getHelper().getExerciseDao().create(exercise);
+		}
+	}
 
 	// Private -------------------------------------------------------
-
+	private boolean assertExercise(Exercise exercise){
+		return assertNotBlankName(exercise) && assertNotDuplicatedName(exercise);
+	}
+	
+	private boolean assertNotBlankName(Exercise exercise) {
+		if (StringUtils.isBlank(exercise.getName())) {
+			Toast.makeText(getApplicationContext(), R.string.exerciseNameBlank,
+					Toast.LENGTH_SHORT).show();
+		}
+		return true;
+	}
+	
+	private boolean assertNotDuplicatedName(Exercise exercise){
+		List<Exercise> tmp = getHelper().getExerciseDao().queryForEq("name", exercise.getName());
+		if (tmp.size() != 0) {
+			Toast.makeText(getApplicationContext(), R.string.exerciseNameDuplicated,
+					Toast.LENGTH_SHORT).show();
+		}
+		return true;
+	}
 	// Inner classes -------------------------------------------------
 }
