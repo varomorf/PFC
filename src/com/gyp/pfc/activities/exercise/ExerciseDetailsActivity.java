1fc8de6c585eecf3d4611af76674199176fa4216
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2013-01-02 14:35:14 +0100

    h5: remove no longer needed import

--- src/com/gyp/pfc/activities/exercise/ExerciseDetailsActivity.java
+++ src/com/gyp/pfc/activities/exercise/ExerciseDetailsActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.gyp.pfc.R;
