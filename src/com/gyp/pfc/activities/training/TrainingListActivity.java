9e2caf464dbf47ce89a1cec7126a681261687a05
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2013-01-07 14:34:04 +0100

    Add backbone for TrainingListActivity.

---
+++ src/com/gyp/pfc/activities/training/TrainingListActivity.java
@@ -0,0 +1,31 @@
+package com.gyp.pfc.activities.training;
+
+import com.gyp.pfc.data.db.DatabaseHelper;
+import com.j256.ormlite.android.apptools.OrmLiteBaseListActivity;
+
+/**
+ * Activity for listing the trainings available on the 
+ * 
+ * @author Alvaro
+ *
+ */
+public class TrainingListActivity extends OrmLiteBaseListActivity<DatabaseHelper>{
+
+	// Constants -----------------------------------------------------
+
+	// Attributes ----------------------------------------------------
+
+	// Static --------------------------------------------------------
+
+	// Constructors --------------------------------------------------
+
+	// Public --------------------------------------------------------
+
+	// Package protected ---------------------------------------------
+
+	// Protected -----------------------------------------------------
+
+	// Private -------------------------------------------------------
+
+	// Inner classes -------------------------------------------------
+}
