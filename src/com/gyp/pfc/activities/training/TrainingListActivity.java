a499b1d3b9db9850a2d76c7d8a6e7a90a6fbe892
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2013-01-10 23:04:12 +0100

    Implement the deletion of trainings.

--- src/com/gyp/pfc/activities/training/TrainingListActivity.java
+++ src/com/gyp/pfc/activities/training/TrainingListActivity.java
@@ -3,6 +3,9 @@ package com.gyp.pfc.activities.training;
 import java.util.List;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.gyp.pfc.R;
 import com.gyp.pfc.adapters.TrainingAdapter;
@@ -29,6 +32,28 @@ public class TrainingListActivity extends
 
 	// Public --------------------------------------------------------
 
+	/**
+	 * Callback for the delete button
+	 * 
+	 * @param view
+	 */
+	public void deleteButton(View view) {
+		// get index item of button pressed
+		int index = getIndexOfItemForPressedButton(view);
+		// get the training from the adapter
+		Training training = getAdapter().getItem(index);
+		// remove related TrainginExercises
+		getHelper().getTrainingExerciseDao().delete(training.getExercises());
+		// remove from DB
+		getHelper().getTrainingDao().delete(training);
+		// remove from UI
+		getAdapter().remove(training);
+		getAdapter().notifyDataSetChanged();
+		// show deletion message
+		Toast.makeText(getApplicationContext(), R.string.trainingDeleted,
+				Toast.LENGTH_SHORT).show();
+	}
+
 	// Package protected ---------------------------------------------
 
 	// Protected -----------------------------------------------------
@@ -43,6 +68,16 @@ public class TrainingListActivity extends
 	}
 
 	// Private -------------------------------------------------------
+
+	private int getIndexOfItemForPressedButton(View view) {
+		// get position of item for which button was pressed
+		ListView exerciseList = (ListView) findViewById(android.R.id.list);
+		return exerciseList.indexOfChild((View) view.getParent());
+	}
+
+	private TrainingAdapter getAdapter() {
+		return (TrainingAdapter) getListAdapter();
+	}
 
 	// Inner classes -------------------------------------------------
 }
