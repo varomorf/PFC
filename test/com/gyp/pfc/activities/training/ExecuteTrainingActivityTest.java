790f2d03a8c4308b26cbb039f79663666c072c21
Alvaro Fernandez Gonzalez <varomorf@gmail.com>
2013-01-14 23:40:28 +0100

    h10: prepare tests for the initial loading of data in the ExecuteTrainingActivity

---
+++ test/com/gyp/pfc/activities/training/ExecuteTrainingActivityTest.java
@@ -0,0 +1,92 @@
+package com.gyp.pfc.activities.training;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import android.app.Activity;
+
+import com.gyp.pfc.CustomTestRunner;
+import com.gyp.pfc.R;
+import com.gyp.pfc.activities.exercise.BaseExerciseTest;
+import com.gyp.pfc.data.domain.Exercise;
+import com.gyp.pfc.data.domain.Training;
+
+@RunWith(CustomTestRunner.class)
+public class ExecuteTrainingActivityTest extends BaseTrainingTest {
+
+	// Constants -----------------------------------------------------
+
+	private static final int SECS = 60;
+	private static final int REPS = 10;
+	private static final String EXERCISE2_NAME = "Foo";
+	private static final String EXERCISE2_DESC = "Bar";
+
+	// Attributes ----------------------------------------------------
+
+	private Training training;
+	private Exercise exercise1;
+	private Exercise exercise2;
+
+	// Static --------------------------------------------------------
+
+	// Constructors --------------------------------------------------
+
+	// Public --------------------------------------------------------
+
+	@Before
+	public void before() {
+		super.before();
+		exercise1 = null;
+		exercise2 = null;
+	}
+
+	@Test
+	public void shouldLoadInitialDataWithIntent() {
+		// GIVEN
+		// one training is passed via intent to the activity
+		passTrainingToActivity();
+		// WHEN
+		// activity is created
+		createActivity();
+		// THEN
+		// training name as title
+		assertViewText(R.id.trainingName, training.getName());
+		// exercise name is shown
+		assertViewText(R.id.exerciseName, exercise1.getName());
+		// seconds of first exercise is shown
+		assertViewText(R.id.timer, "01:00");
+		// reps of first exercise is shown
+		assertViewText(R.id.repetitionNumber, activity.getText(R.string.repetition) + " 1/10");
+		// total exercises are shown
+		assertViewText(R.id.exerciseNumberFraction, activity.getText(R.string.exercise_label) + " 1/2");
+		// button shows resume/pause text
+		assertViewText(R.id.actionButton, activity.getText(R.string.resumePause).toString());
+	}
+
+	// Package protected ---------------------------------------------
+
+	// Protected -----------------------------------------------------
+
+	@Override
+	protected Activity newActivity() {
+		return new ExecuteTrainingActivity();
+	}
+
+	// Private -------------------------------------------------------
+
+	private void passTrainingToActivity() {
+		training = createTraining(TRAINING_NAME);
+		BaseExerciseTest.insertExercise(exerciseDao,
+				BaseExerciseTest.EXERCISE_NAME, BaseExerciseTest.EXERCISE_DESC);
+		BaseExerciseTest.insertExercise(exerciseDao, EXERCISE2_NAME,
+				EXERCISE2_DESC);
+		exercise1 = exerciseDao.queryForId(1);
+		addExerciseToTraining(training, exercise1, REPS, SECS);
+		exercise2 = exerciseDao.queryForId(2);
+		addExerciseToTraining(training, exercise2, REPS + 1, SECS + 1);
+		intentPassedWithTraining(1);
+	}
+
+	// Inner classes -------------------------------------------------
+}
