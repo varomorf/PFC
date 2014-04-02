6750f2f04b05e57b97e4f20ad596ce8c9d56482a
Alvaro Fernandez González <varomorf@gmail.com>
2014-04-02 12:40:03 +0200

    Add test data for Food and Exercise entities.

--- app/src/main/java/com/gyp/pfc/activities/MainActivity.java
+++ app/src/main/java/com/gyp/pfc/activities/MainActivity.java
@@ -6,6 +6,9 @@ import android.view.Menu;
 import com.gyp.pfc.R;
 import com.gyp.pfc.adapters.MainListAdapter;
 import com.gyp.pfc.data.db.DatabaseHelper;
+import com.gyp.pfc.data.domain.manager.ExerciseManager;
+import com.gyp.pfc.data.domain.manager.FoodManager;
+import com.gyp.pfc.data.domain.manager.TrainingManager;
 import com.j256.ormlite.android.apptools.OrmLiteBaseListActivity;
 
 /**
@@ -27,9 +30,13 @@ public class MainActivity extends OrmLiteBaseListActivity<DatabaseHelper> {
 	// Constructors --------------------------------------------------
 
 	// Public --------------------------------------------------------
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		initializeManagers();
+		// TODO change me
+		loadTestData();
 		// set the list adapter
 		MainListAdapter adapter = new MainListAdapter(this,
 				R.layout.main_list_item, MAIN_SECTIONS_NAMES);
@@ -47,7 +54,46 @@ public class MainActivity extends OrmLiteBaseListActivity<DatabaseHelper> {
 
 	// Protected -----------------------------------------------------
 
+	protected void loadTestData() {
+		// load test food
+		FoodManager.getInstance().createFood("Arroz", "", 364, 6.67, 81.60, 0,
+				1.4, 0.9, 0.19, 0.0039);
+		FoodManager.getInstance().createFood("Huevo", "", 162, 12.68, 0.68, 0,
+				0, 12.10, 3.3, 0.144);
+		FoodManager.getInstance().createFood("Pan blanco", "Mercadona", 261,
+				8.47, 51.50, 0, 3.5, 1.6, 0.39, 0.540);
+		// load test exercises
+		ExerciseManager
+				.getInstance()
+				.createExercise(
+						"Planchas",
+						"Boca abajo, apoyamos las manos contra el suelo a la altura de los hombros y elevamos el cuerpo mediante una extensión de los brazos. Mediante una flexión controlada, volvemos a la posición de partida.");
+		ExerciseManager
+				.getInstance()
+				.createExercise(
+						"Abdominales",
+						"Boca arriba, flexionamos las piernas de modo que los pies toquen el suelo. Elevamos el tronco hasta erguirnos. Bajamos el tronco a la posición de partida de forma controlada.");
+		ExerciseManager
+				.getInstance()
+				.createExercise(
+						"Zancadas",
+						"De pie, damos un paso con una pierna, dejando la otra en el sitio. Flexionamos la rodilla adelantada, de tal modo que la rodilla atrasada llegue a tocar el suelo. Volvemos a la posición de partida.");
+		// load test trainings
+		TrainingManager.getInstance().createTraining("Serie de abdominales");
+	}
+
 	// Private -------------------------------------------------------
+
+	/**
+	 * Initializes all managers
+	 */
+	private void initializeManagers() {
+		TrainingManager.getInstance().setTrainingDao(
+				getHelper().getTrainingDao());
+		FoodManager.getInstance().setFoodDao(getHelper().getFoodDao());
+		ExerciseManager.getInstance().setExerciseDao(
+				getHelper().getExerciseDao());
+	}
 
 	// Inner classes -------------------------------------------------
 
