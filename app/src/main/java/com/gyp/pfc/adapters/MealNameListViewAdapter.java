f1d04cb15dd6d7ef5ca656d372bbe5aebeda2b85
Alvaro Fernandez González <varomorf@gmail.com>
2014-06-16 23:23:16 +0200

    Make MealNameListViewAdapter return correct Views for spinner

--- app/src/main/java/com/gyp/pfc/adapters/MealNameListViewAdapter.java
+++ app/src/main/java/com/gyp/pfc/adapters/MealNameListViewAdapter.java
@@ -6,9 +6,13 @@ package com.gyp.pfc.adapters;
 import java.util.List;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.gyp.pfc.R;
+import com.gyp.pfc.UIUtils;
 import com.gyp.pfc.data.domain.MealName;
 
 /**
@@ -22,6 +26,8 @@ public class MealNameListViewAdapter extends ArrayAdapter<MealName> {
 	// Constants -----------------------------------------------------
 
 	// Attributes ----------------------------------------------------
+
+	private LayoutInflater inflater = null;
 
 	// Static --------------------------------------------------------
 
@@ -39,11 +45,32 @@ public class MealNameListViewAdapter extends ArrayAdapter<MealName> {
 	 * @param objects
 	 *            The objects to represent in the ListView.
 	 */
-	public MealNameListViewAdapter(Context context, int textViewResourceId, List<MealName> names) {
-		super(context, textViewResourceId, R.id.name, names);
+	public MealNameListViewAdapter(Context context, List<MealName> names) {
+		super(context, R.layout.meal_name_list_item, R.id.name, names);
+		inflater = LayoutInflater.from(context);
 	}
 
 	// Public --------------------------------------------------------
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		View view = convertView;
+		if (view == null) {
+			// generate view inflating the layout
+			view = inflater.inflate(R.layout.meal_name_list_item, null);
+		}
+
+		// populate the item with the data
+		MealName name = getItem(position);
+		UIUtils.setTextToUI(view.findViewById(R.id.name), name.getName());
+
+		return view;
+	}
+
+	@Override
+	public View getDropDownView(int position, View convertView, ViewGroup parent) {
+		return getView(position, convertView, parent);
+	}
 
 	// Package protected ---------------------------------------------
 
